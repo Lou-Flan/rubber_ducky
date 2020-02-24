58b931dbe367ef936c4e091b3978a854c1e137fa
Lou-Flan <louise_flanagan@outlook.com>
2020-02-24 15:47:35 +1100

    faker gem and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+for i in 1..10
+    Listing.create(
+        name: Faker::DcComics.hero,
+        description: Faker::Lorem.paragraph(sentence_count: rand(2..5)),
+        price: Faker::Number.between(from: 1, to: 20)
+        )
+    puts "Created #{i} listings"
+end
