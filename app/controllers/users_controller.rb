f5c31339ee0dcd594941d1cc67ff1409f9114c25
Louise <louise_flanagan@outlook.com>
2020-02-27 17:06:02 +1100

    edit profile pages working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
-    
+    before_action :set_profile, only: [:my_profile, :edit, :update]
+    before_action :authenticate_user!
+
     def my_profile
+        console
+    end
+
+    def update
+        if @user.update(profile_params)
+            redirect_to @user
+        else
+         render "edit"
+        #  FIX ME ADD ERROR MESSAGING  
+        end
+    end
+
+    def edit
+    end
+
+
+
+private
+
+    def set_profile
         @user = current_user
+    end
+
+    def profile_params
+        params.require(:user).permit(:bio)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :listings
   get "/", to: "pages#home", as: :root
-  get "/profile", to: "users#my_profile", as: :profile
+  get "/profile", to: "users#my_profile", as: "user"
 
+  patch "/profile", to: "users#update"
+  put  "/profile", to: "users#update"
+  get "/profile/edit", to: "users#edit", as: "edit_user"
+  
   get "/:path", to: "pages#not_found"
   
 end
