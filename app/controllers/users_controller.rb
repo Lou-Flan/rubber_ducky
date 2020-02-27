6e75d29de968f1ef9e9d4ec33fa1e015118485db
Louise <louise_flanagan@outlook.com>
2020-02-27 15:55:51 +1100

    created users controller, added page to view user profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+    
+    def my_profile
+        @user = current_user
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'page/not_found'
   devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :listings
-get "/", to: "pages#home", as: :root
-get "/:path", to: "pages#not_found"
+  resources :listings
+  get "/", to: "pages#home", as: :root
+  get "/profile", to: "users#my_profile", as: :profile
+
+  get "/:path", to: "pages#not_found"
+  
 end
